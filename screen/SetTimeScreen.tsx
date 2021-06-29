28efc239271e2b37cd82f2897f1788bef0d7af03
jaeyoung <66120479+kwakjy@users.noreply.github.com>
2021-06-29 16:47:16 +0900

    react-native-picker/picker 설치
    Picker사용을 위한 react-native-picker/picker 설치

--- screen/SetTimeScreen.tsx
+++ screen/SetTimeScreen.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import {View, Text, Button} from 'react-native'
+import React, {useState} from 'react'
+import {View, Text, Button, StyleSheet} from 'react-native'
+import {Picker} from '@react-native-picker/picker';
 import {RootStackParamList} from '../navigation/ParamList'
 import {StackNavigationProp} from '@react-navigation/stack'
 import {RouteProp} from '@react-navigation/native'
@@ -16,6 +17,12 @@ export const SetTimeScreen: React.FC<Props> = (Props) => {
   const {navigation, route} = Props
   const whatScreen = route.params.screen
 
+  const [text, setText] = useState('');
+
+  const onChangeText = (value: string) => {
+    setText(value);
+  };
+
   const backScreen = () => {
     if (whatScreen === 'Home')
       navigation.navigate('Home')
@@ -25,10 +32,32 @@ export const SetTimeScreen: React.FC<Props> = (Props) => {
 
   return (
     <View>
+      <Text>이미지 하나가 보여질 시간(1~10초)을 선택하세요.</Text>
+      <Picker
+        style={styles.pickerSet}
+        selectedValue={text}
+        onValueChange={value => onChangeText(value)}>
+        <Picker.Item label="1초" value="1" />
+        <Picker.Item label="2초" value="2" />
+        <Picker.Item label="3초" value="3" />
+        <Picker.Item label="4초" value="4" />
+        <Picker.Item label="5초" value="5" />
+        <Picker.Item label="6초" value="6" />
+        <Picker.Item label="7초" value="7" />
+        <Picker.Item label="8초" value="8" />
+        <Picker.Item label="9초" value="9" />
+        <Picker.Item label="10초" value="10" />
+      </Picker>
       <Button title='원래화면으로'
         onPress={backScreen}
       />
-      <Text>{whatScreen}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  pickerSet: {
+    width: 70,
+    height: 150,
+  }
+})
