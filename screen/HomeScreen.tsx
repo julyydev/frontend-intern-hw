6c67d31f13e0987fb62d24520b231c595377b24a
jaeyoung <66120479+kwakjy@users.noreply.github.com>
2021-06-29 18:20:19 +0900

    HomeScreen 1차 디자인
    HomeScreen 간단하게 디자인

--- screen/HomeScreen.tsx
+++ screen/HomeScreen.tsx
@@ -1,8 +1,11 @@
 import React, {useState} from 'react'
-import {View, Text, Button, Alert} from 'react-native'
+import {View, Text, Button, Alert, StyleSheet, Image} from 'react-native'
 import {RootStackParamList} from '../navigation/ParamList'
 import {StackNavigationProp} from '@react-navigation/stack'
 import {RouteProp} from '@react-navigation/native'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import FlickrLogo from '../image/FlickrLogo.png'
 
 type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>
 type HomeScreenRouteProp = RouteProp<RootStackParamList, 'Feed'>
@@ -38,15 +41,60 @@ export const HomeScreen: React.FC<Props> = (Props) => {
   }
 
   return (
-    <View>
-      <Text>현재 슬라이드 시간(초)</Text>
-      <Text>{second}{isFirst ? ' ' : '초'}</Text>
-      <Button title='슬라이드 시간 변경'
-        onPress={moveSetTimeScreen}
-      />
-      <Button title='슬라이드 시작'
-        onPress={moveFeedScreen}
-      />
+    <View style={styles.mainView}>
+      <View style={styles.section1View}>
+        {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
+        <Image style={styles.imageSet} source={(FlickrLogo)} />
+        <Text style={styles.titleSet}>Album App</Text>
+      </View>
+      <View style={styles.section2View}>
+        <Text>현재 슬라이드 시간(초)</Text>
+        <Text>{second}{isFirst ? ' ' : '초'}</Text>
+        <Button title='슬라이드 시간 변경'
+          onPress={moveSetTimeScreen}
+        />
+        <Button title='슬라이드 시작'
+          onPress={moveFeedScreen}
+        />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+    backgroundColor: 'white',
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  section1View: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  section2View: {
+    flex: 1.7,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  titleSet: {
+    fontSize: 40,
+    fontWeight: 'bold'
+  },
+  buttonSet: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imageSet: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    resizeMode: 'contain',
+    width: 100,
+    height: 50
+  },
+  textSet: {
+    fontFamily: 'Helvetica'
+  }
+})
