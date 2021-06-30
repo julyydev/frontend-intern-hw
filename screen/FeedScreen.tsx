ca7c9656906dd37013369e12b0b0621650132005
jaeyoung <66120479+kwakjy@users.noreply.github.com>
2021-06-30 14:32:47 +0900

    Fade in/out 알고리즘 변경
    이미지 간의 딜레이와 중복 이미지를 제거하기 위해 기존 방식에서 이미지 주소를 미리 불러 와서 배열에 저장하고, 이를 통해 사진을 출력하는 방식으로 변경(자현님 도움)

--- screen/FeedScreen.tsx
+++ screen/FeedScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, {useState, useEffect, useRef, useMemo, useCallback} from 'react'
 import {View, Text, Button, Image, Animated, StyleSheet} from 'react-native'
 import {RootStackParamList} from '../navigation/ParamList'
 import {StackNavigationProp} from '@react-navigation/stack'
@@ -6,7 +6,7 @@ import {RouteProp} from '@react-navigation/native'
 
 const Flickr_API =
     'https://api.flickr.com/services/feeds/photos_public.gne?tags=landscape,portrait&tagmode=any' +
-    '&format=json&nojsoncallback=1';
+    '&format=json&nojsoncallback=1'
 
 type FeedScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Feed'>
 type FeedScreenRouteProp = RouteProp<RootStackParamList, 'Feed'>
@@ -16,46 +16,74 @@ type Props = {
   route: FeedScreenRouteProp
 }
 
+interface Media {
+  m: string
+}
+
+interface Item {
+  media: Media
+}
+
+interface Response {
+  items: Array<Item>
+}
+
 export const FeedScreen: React.FC<Props> = (Props) => {
   const {navigation, route} = Props
 
   const second = route.params?.second
-  const fadeAnim = useRef(new Animated.Value(0)).current;
-  const [image, setImage] = useState();
-  const [index, setIndex] = useState(0);
+  const fadeAnim = useRef(new Animated.Value(0)).current
+  const [imageArray, setImageArray] = useState<Array<string>>([])
+  const [isFetchNeeded, setFetchNeeded] = useState(true)
 
   const fadeIn = Animated.timing(fadeAnim, {
     toValue: 1,
     duration: 1000,
     useNativeDriver: true,
-  });
+  })
 
   const fadeOut = Animated.timing(fadeAnim, {
     toValue: 0,
     duration: 1000,
     useNativeDriver: true,
-  });
+  })
+
+  const fetchImage = useCallback(async (): Promise<Array<string>> => {
+    const response = await fetch(Flickr_API)
+    const json: Response = (await response.json()) as Response
+    return json.items.map((item) => item.media.m)
+  }, [])
 
   useEffect(() => {
-    if (index >= 20) {
-      setIndex(0);
+    if (isFetchNeeded) {
+      void fetchImage()
+        .then((newImageArray) => {
+          setImageArray((current) => {
+            current.push(...newImageArray)
+            return current
+          })
+          setFetchNeeded(false)
+        })
     }
-    void fetch(Flickr_API)
-      .then(response => {
-        return response.json();
-      })
-      .then(j => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        setImage(j.items[index].media.m);
-      })
-      .then(() => {
-        Animated.sequence([fadeIn, Animated.delay(second * 1000), fadeOut]).start(
-          () => {
-            setIndex(index + 1);
-          },
-        );
-      });
-  }, [index]);
+  }, [isFetchNeeded])
+
+  const imageView = useMemo(() => {
+    if (imageArray.length === 0) {
+      return <></>
+    }
+    return <Image style={styles.imageSet} source={{uri: imageArray[0]}} />
+  }, [imageArray])
+
+  useEffect(() => {
+    Animated.sequence([fadeIn, Animated.delay(second * 1000), fadeOut]).start(
+      () => {
+        if (imageArray.length === 5) {
+          setFetchNeeded(true)
+        }
+        setImageArray(imageArray.slice(1))
+      }
+    )
+  }, [imageArray.length])
 
   return (
     <View style={styles.mainView}>
@@ -66,7 +94,7 @@ export const FeedScreen: React.FC<Props> = (Props) => {
             opacity: fadeAnim,
           },
         ]}>
-        <Image style={styles.imageSet} source={{uri: image}} />
+        {imageView}
       </Animated.View>
       <View style={styles.section2View}>
         <Text>현재 슬라이드 시간(초)</Text>
