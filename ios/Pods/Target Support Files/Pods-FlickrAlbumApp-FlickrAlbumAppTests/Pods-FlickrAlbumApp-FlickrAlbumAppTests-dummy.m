25d5bda84f7d1509e88e72c7b8a7634d9786ebe8
Jahyun Oh <jahyun@rapportlabs.kr>
2021-07-07 12:32:04 +0900

    Remove ignore files for ios

--- ios/Pods/Target Support Files/Pods-FlickrAlbumApp-FlickrAlbumAppTests/Pods-FlickrAlbumApp-FlickrAlbumAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FlickrAlbumApp_FlickrAlbumAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_FlickrAlbumApp_FlickrAlbumAppTests
-@end
